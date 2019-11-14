eb1a7c195e1275a6848d42fc5f87fc1caf3aa01f
Grant <gmontgomery1994@gmail.com>
2019-11-14 12:27:16 -1000

    still trying to set up yelp api call within SearchBox component.

--- my-app/src/components/SearchBox/SearchBox.jsx
+++ my-app/src/components/SearchBox/SearchBox.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import {AppContext} from "../../AppContext"
+// import {AppContext} from "../../AppContext"
 import "./SearchBox.css"
+
+require("dotenv").config()
 
 
 class SearchBox extends Component {
@@ -8,15 +10,51 @@ class SearchBox extends Component {
         super(props)
         this.state={
             term: "",
-            location: "",
-            radius: null
+            location: ""
         }
     }
 
     updateTextInput = (event) => {
-        this.setState({term: event.target.value})
-        console.log(this.state.term)
+       this.setState({[event.target.name]: event.target.value})
     }
+
+    // makeCall = () => {
+    //     const {term, location} = this.state
+    //     const yelp = new URL ("https://api.yelp.com/v3/businesses/search"),
+    //     params = {term: term, location: location, radius: 10000}
+
+    //     Object.keys(params).forEach(key => yelp.searchParams.append(key, params[key]))
+
+    //     const response = fetch(yelp, {
+    //     headers: {
+    //     Authorization: `Bearer ${process.env.REACT_APP_YELP_API_KEY}`
+    //     }
+    //         })
+
+    //     response.then(resp => resp.json())
+    //     .then(data => console.log(data.businesses))
+    //     .catch(error => console.log(error.message))
+    // }
+
+    // onHandleSubmit = (event) => {
+    //     event.preventDefault()
+    //     this.makeCall()
+    // }
+
+    // addTask = () => {
+    //     if(this.state.newTodo.value === ""){
+    //         alert("Must enter in a task.")
+    //     }
+    //     else{
+    //         this.setState({
+    //             todos: [...this.state.todos, this.state.newTodo]
+    //         })
+    //         console.log(this.state.newTodo)
+    //         this.setState({newTodo: {id: null, value: ""}})
+    //     }
+    // }
+
+
 
     render() { 
         return (
@@ -25,18 +63,20 @@ class SearchBox extends Component {
                 <form action="">
                     <label htmlFor="">Search</label>
                     <input type="text"
+                    name="term"
                     value={this.state.term}
                     onChange={e => this.updateTextInput(e)}
                     />
                     <br/>
                     <label htmlFor="">Location</label>
-                    <input type="text"
-                    location={this.state.location}/>
+                    <input name="location"
+                    type="text"
+                    value={this.state.location}
+                    onChange={e => this.updateTextInput(e)}/>
                     <br/>
-                    <label htmlFor="">Radius</label>
-                    <select name="" id="">
-                        <option value=""></option>
-                    </select>
+                    <button onClick={() => console.log(process.env.REACT_APP_YELP_API_KEY)}>
+                        Submit
+                    </button>
                 </form>
             </div>
             
