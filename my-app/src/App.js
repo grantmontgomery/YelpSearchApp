7d4d4651c4fc7e04947ffc945be36e09a57c0fc1
Grant <gmontgomery1994@gmail.com>
2019-11-15 12:51:43 -1000

    starting over to learn Express in order to implement it in app and finally use it to work around cors issue.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,41 +8,15 @@ require("dotenv").config()
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-          name: '',
-          greeting: ''
+
         };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-      }
-    
-      handleChange(event) {
-        this.setState({ name: event.target.value });
-      }
-    
-      handleSubmit(event) {
-        event.preventDefault();
-        fetch(`/api/greeting?name=${encodeURIComponent(this.state.name)}`)
-          .then(response => response.json())
-          .then(state => console.log(state))
-        //   .then(state => this.setState(state));
-      }
+  
+
     render() { 
         return (
-            <div>
-                  <form onSubmit={this.handleSubmit}>
-            <label htmlFor="name">Enter your name: </label>
-            <input
-              id="name"
-              type="text"
-              value={this.state.name}
-              onChange={this.handleChange}
-            />
-            <button type="submit">Submit</button>
-          </form>
-          <p>{this.state.greeting}</p>
-            </div>
-          
+          <div>
+            starting over
+          </div>
             
           );
     }
