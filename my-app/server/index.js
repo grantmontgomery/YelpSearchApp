6099613dec0f224a775bff6025f69234a52ffb8a
Grant <gmontgomery1994@gmail.com>
2019-11-16 10:45:05 -1000

    small commit

--- my-app/server/index.js
+++ my-app/server/index.js
@@ -1,4 +1,15 @@
 const express = require('express');
 
+const app = express()
 
+app.get("/", (req, res) => {
+    res.send(
+        
+        "<h1>Hallo Welt</h1>"
+        
+    )
+})
 
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log(`Server started on ${PORT}`))
